58e10112b8a8db5d4a196d10dcc6559502e9c994
ALLAN VERGILIO FELIZZETI <alllanvergilio@gmail.com>
2023-05-07 10:56:00 -0300

    Criando logicar de Insert, Update e Delete de Empresas

--- api/desafioRest/src/app/controller/EmpController.ts
+++ api/desafioRest/src/app/controller/EmpController.ts
@@ -1,13 +1,60 @@
-import { Request, Response, Router } from 'express'
-import Empresa from '../entities/Emp'
-import EmpRepository from '../repositories/EmpRepository'
-import IUemp from '../interfaces/IUemp'
+import { Request, Response, Router } from 'express';
+import Empresa from '../entities/Emp';
+import EmpRepository from '../repositories/EmpRepository';
+import IUemp from '../interfaces/IUemp';
 
 const empRouter = Router();
 
-empRouter.get('/', async (_req : Request, res: Response): Promise<Response> => {
+empRouter.get('/', async (_req: Request, res: Response): Promise<Response> => {
   const emps = await EmpRepository.getEmps();
   return res.status(200).json(emps);
-})
+});
+
+empRouter.post('/', async (req: Request, res: Response): Promise<Response> => {
+  const { razao_social, cnpj, cep, cidade, estado, bairro, complemento } = req.body;
+
+  const newEmp: IUemp = {
+    razao_social,
+    cnpj,
+    cep,
+    cidade,
+    estado,
+    bairro,
+    complemento,
+  };
+
+  const emp = await EmpRepository.createEmp(newEmp);
+
+  return res.status(201).json(emp);
+});
+
+empRouter.put('/:id', async (req: Request, res: Response): Promise<Response> => {
+  const id = parseInt(req.params.id);
+  const { razao_social, cnpj, cep, cidade, estado, bairro, complemento } = req.body;
+
+  const updatedEmp: IUemp = {
+    razao_social,
+    cnpj,
+    cep,
+    cidade,
+    estado,
+    bairro,
+    complemento,
+  };
+
+  const emp = await EmpRepository.updateEmp(id, updatedEmp);
+
+  if (!emp) {
+    return res.status(404).json({ message: 'Empresa não encontrada.' });
+  }
+
+  return res.status(200).json(emp);
+});
+
+empRouter.delete('/:id', async (req: Request, res: Response): Promise<Response> => {
+  const id = parseInt(req.params.id);
+  await EmpRepository.deleteEmp(id);
+  return res.status(204).send();
+});
 
 export default empRouter;
--- api/desafioRest/src/app/repositories/EmpRepository.ts
+++ api/desafioRest/src/app/repositories/EmpRepository.ts
@@ -8,4 +8,26 @@ const getEmps = (): Promise<IUemp[]> => {
   return empresaRepository.find();
 }
 
-export default { getEmps };
+const createEmp = (emp: IUemp): Promise<Empresa> => {
+  const newEmp = empresaRepository.create(emp);
+  return empresaRepository.save(newEmp);
+}
+
+const updateEmp = async (id: number, emp: IUemp): Promise<Empresa | null> => {
+  const existingEmp = await empresaRepository.findOne({ where: { id } });
+
+  if (!existingEmp) {
+    return null;
+  }
+
+  const updatedEmp = empresaRepository.merge(existingEmp, emp);
+
+  return empresaRepository.save(updatedEmp);
+}
+
+const deleteEmp = async (id: number): Promise<boolean> => {
+  const result = await empresaRepository.delete({ id });
+  return result.affected !== 0;
+}
+
+export default { getEmps, createEmp, updateEmp, deleteEmp };
